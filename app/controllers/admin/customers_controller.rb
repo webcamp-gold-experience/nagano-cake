63bd44d134c746620ad47f3847d3701729b9735b
shoponcom001 <shopon.com001@gmail.com>
2021-08-17 12:29:55 +0000

    [Update]会員作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,11 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+ 
   def index
-    @customers = customer.all
+    @customers = Customer.all
   end
 
   def show
-    @customer = Customer.find(params[])
   end
 
   def edit
