564c041542b00a2d4c55b54432901002a023e017
konmarii <chankonworld@gmail.com>
2021-08-17 02:28:16 +0000

    [Add]カート内商品一覧画面のテーブル

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,8 +1,14 @@
 class Public::CartProductsController < ApplicationController
   def index
+    @cart_products = CartProduct.where(customer_id: current_customer.id)
   end
   
   def create
+    @cart_product = CartProduct.new(cart_product_params)
+    @cart_prodcut.customer_id = current_customer.id
+    if @cart_prodcut.save
+      redirect_to cart_products_path
+    end  
   end
 
   def update
@@ -12,5 +18,10 @@ class Public::CartProductsController < ApplicationController
   end
   
   def destroy_all
+  end
+  
+  private
+  def cart_product_params
+    params.require(:cart_product).permit(:product_id, :amount)
   end
 end
