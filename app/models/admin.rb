d303cd10d830abf14b86af91ae8a5dab7bf1b54c
shoponcom001 <shopon.com001@gmail.com>
2021-08-17 14:01:33 +0000

    [Add]会員一覧作成

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :validatable
+
+  enum is_deleted: { 有効: true, 退会: false }
+
+
 end
