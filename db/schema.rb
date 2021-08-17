153cc7852af5180356d35a473cd3703a50ab9e4a
konmarii <chankonworld@gmail.com>
2021-08-17 06:23:25 +0000

    [Add]ヘッダー

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_061155) do
+ActiveRecord::Schema.define(version: 2021_08_17_002406) do
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "custmer_id", null: false
+    t.integer "customer_id", null: false
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
@@ -21,12 +21,44 @@ ActiveRecord::Schema.define(version: 2021_08_14_061155) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id", null: false
-    t.integer "custmer_id", null: false
+    t.integer "customer_id", null: false
     t.integer "amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_deleted", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
@@ -40,20 +72,20 @@ ActiveRecord::Schema.define(version: 2021_08_14_061155) do
     t.integer "product_id", null: false
     t.integer "tax_price", null: false
     t.integer "amount", null: false
-    t.integer "production_status", null: false
+    t.integer "production_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "custmer_id", null: false
+    t.integer "customer_id", null: false
     t.string "delivary_postal_code", null: false
     t.string "delivary_address", null: false
     t.string "delivary_name", null: false
     t.integer "delivary_fee", null: false
     t.integer "total_payment", null: false
-    t.integer "order_status", null: false
-    t.integer "payment_method", null: false
+    t.integer "order_status", default: 0, null: false
+    t.integer "payment_method", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -64,7 +96,7 @@ ActiveRecord::Schema.define(version: 2021_08_14_061155) do
     t.text "introduction", null: false
     t.string "image_id", null: false
     t.integer "price", null: false
-    t.integer "sales_status", null: false
+    t.integer "sales_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
