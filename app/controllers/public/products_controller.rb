7d307008cc73188bef5e06c12ee4be14e5519421
tarooooooo <ryutaromaeda83@gmail.com>
2021-08-17 23:07:21 +0000

    [Add]top

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,8 +1,12 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Product.where(sales_status:0)
+    @genres = Genre.all
   end
 
   def show
+    @product = Product.find(params[:id])
+    @genres = Genre.all
   end
 
   def search
