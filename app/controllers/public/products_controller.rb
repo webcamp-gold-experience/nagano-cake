ed9d04ce3a6f1db64cb2ee4796fddb69a01331c9
tarooooooo <ryutaromaeda83@gmail.com>
2021-08-18 04:20:41 +0000

    [Fix]ジャンル検索の修正

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -2,6 +2,7 @@ class Public::ProductsController < ApplicationController
   def index
     @products = Product.where(sales_status:0)
     @genres = Genre.all
+
   end
 
   def show
@@ -10,5 +11,9 @@ class Public::ProductsController < ApplicationController
   end
 
   def search
+    @products = Product.where(" genre_id = ? and sales_status = ? ", params[:id], 0 )
+    @genres = Genre.all
+    @genre = Genre.find(params[:id])
+    render :index
   end
 end
