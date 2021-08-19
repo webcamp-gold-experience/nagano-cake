7f42aace60cc72831c5a57a51030de792b1185ed
konmarii <chankonworld@gmail.com>
2021-08-19 14:16:05 +0000

    [add]情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,8 @@
 class Public::OrdersController < ApplicationController
   #before_action :authenticate_customer!
-  
+
   def new
+    @order = Order.new
   end
 
   def confirm
@@ -18,9 +19,14 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-  
+
   def authenticate_customer
     @customer != current_customer
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :delivary_postal_code, :delivary_address, :delivary_name, :delivary_fee, :total_payment, :order_status, :payment_method)
+  end
 end
