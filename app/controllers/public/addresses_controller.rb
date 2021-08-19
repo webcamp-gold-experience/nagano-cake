86f0323631ea7cc668cacd67f95f2fbfd0c59787
shoponcom001 <shopon.com001@gmail.com>
2021-08-19 08:33:05 +0000

    [Add]配送先実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,5 @@
 class Public::AddressesController < ApplicationController
-  #before_action :authenticate_customer!
+  before_action :authenticate_customer!
 
   def index
     @address = Address.new
@@ -11,16 +11,26 @@ class Public::AddressesController < ApplicationController
     @address.customer_id = current_customer.id
     if @address.save
       redirect_to addresses_path
+    else
+      @addresses = Address.where(customer_id: current_customer.id)
+      render :index
     end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
   private
