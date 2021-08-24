23097ecde5fbc69f5627daf5f4450383f34a8f9c
konmarii <chankonworld@gmail.com>
2021-08-24 07:53:30 +0000

    一時保存

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,43 @@ $(document).ready(function () {
   });
 });
 
+// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
+function TextTypingAnime() {
+  $('.TextTyping').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var thisChild = ""; {
+      thisChild = $(this).children();
+      thisChild.each(function () {
+        $(this).stop(); //delay処理を止める
+        $(this).css("display", "none"); //spanタグ非表示
+      });
+    }
+  });
+}
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".TextTyping");
+  element.each(function () {
+    var text = $(this).html();
+    var textbox = "";
+    text.split('').forEach(function (t) {
+      if (t !== " ") {
+        textbox += '<span>' + t + '</span>';
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+
+  });
+
+  TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
