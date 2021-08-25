750d1e391e5e060eed81ecb3054fa3e0de5d65bf
shoponcom001 <shopon.com001@gmail.com>
2021-08-25 04:46:14 +0000

    [Update] ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,9 +2,19 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    order_product = OrderProduct.find(params[:id])
-    order_product.update(order_product_params)
-    redirect_to admin_order_path(order_product.order)
+    @order_product = OrderProduct.find(params[:id])
+    @order_product.update(order_product_params)
+    @order = @order_product.order
+    @order_products = @order.order_products
+    @status = @order_products.pluck(:production_status)
+    if @order_product.production_status == "during_production"
+      @order.order_status = "in_production"
+      @order.save
+    elsif @status.all?{|status|status == "production_completed"}
+      @order.order_status = "preparing_delivery"
+      @order.save
+    end
+    redirect_to request.referer
   end
 
   private
